61d55b80867a8ecc488799bda9ef38b1160e1f4c
jnenriqu1 <94874275+jnenriqu1@users.noreply.github.com>
2021-11-24 20:42:49 -0500

    Update DemoOne.java
    added println from GitHub

--- src/dayOne/DemoOne.java
+++ src/dayOne/DemoOne.java
@@ -4,6 +4,7 @@ public class DemoOne {
 
 	public static void main(String[] args) {
 		System.out.println("first attempt at GitHub");
+		System.out.println("first attempt at Pull println from GitHUb);
 	}
 
 	public static void subNum(int x, int y) {
