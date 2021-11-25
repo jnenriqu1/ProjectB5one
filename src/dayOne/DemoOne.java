71f8ae83cd7ef107a12c0cffd4f99aa5fc094acd
jnenr <jnenr@DESKTOP-TUIGB2O.myfiosgateway.com>
2021-11-24 20:02:21 -0500

    added second method

--- src/dayOne/DemoOne.java
+++ src/dayOne/DemoOne.java
@@ -5,4 +5,8 @@ public class DemoOne {
 	public static void main(String[] args) {
 		System.out.println("first attempt at GitHub");
 	}
+
+	public static void subNum(int x, int y) {
+		System.out.println(x - y);
+	}
 }
