8a39e66a5ec80facc34b09fd67b7a116a17d9d71
jnenr <jnenr@DESKTOP-TUIGB2O.myfiosgateway.com>
2021-11-28 18:09:13 -0500

    added comment..

--- src/dayOne/DemoOne.java
+++ src/dayOne/DemoOne.java
@@ -11,6 +11,7 @@ public class DemoOne {
 	public static void subNum(int x, int y) {
 		System.out.println(x - y);
 
+		// added for loop
 		for (int i = 0; i >= 10; i++) {
 			System.out.println(i);
 		}
